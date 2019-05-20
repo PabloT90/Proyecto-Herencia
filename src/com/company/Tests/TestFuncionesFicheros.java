213449bac84a5cfcec48696a1b380ec609f68141
Pablo <pabloprats18@gmail.com>
2019-05-20 17:30:19 +0200

    toma tonto

--- src/com/company/Tests/TestFuncionesFicheros.java
+++ src/com/company/Tests/TestFuncionesFicheros.java
@@ -16,17 +16,17 @@ public class TestFuncionesFicheros {
         CampamentoImpl[] array = null;
 
         //crearFicheroBinario
-        System.out.println(funcion.crearFicheroBinario("Prueba.dat"));
+        System.out.println(funcion.crearFicheroBinario("Prueba2.dat"));
 
         //Insertamos un par de campamentos en el fichero
         CampamentoImpl c1 = new CampamentoImpl("Angola","Tartulia", 1, 1,'O','N');
-        CampamentoImpl c2 = new CampamentoImpl("Afghan","King", 39, 7,'I','P');
+        CampamentoImpl c2 = new CampamentoImpl("Zfghan","King", 39, 7,'I','P');
         CampamentoImpl c3 = new CampamentoImpl("Afghan","Asta", 23, 4,'L','Q');
 
         FileOutputStream fos = null;
         Clases.MyObjectOutputStream moos = null;
         try{
-            fos = new FileOutputStream("Prueba.dat", true);
+            fos = new FileOutputStream("Prueba2.dat", true);
             moos = new MyObjectOutputStream(fos);
             moos.writeObject(c1);
             moos.writeObject(c2);
@@ -45,16 +45,18 @@ public class TestFuncionesFicheros {
         }
 
         //volcarFicheroEnArray
-        array = funcion.volcarFicheroEnArray("Prueba.dat");
+        array = funcion.volcarFicheroEnArray("Prueba2.dat");
         for(int i = 0; i < array.length; i++){
             System.out.println(array[i].toString());
         }
+        System.out.println("Fin volcar fichero en array");
 
         //numeroRegistrosFichero
-        System.out.println(funcion.numeroRegistrosFichero("Prueba.dat"));
+        System.out.println(funcion.numeroRegistrosFichero("Prueba2.dat"));
+        System.out.println("Fin numeroRegistrosFichero");
 
         //mostrarFichero
-        funcion.mostrarFichero("Prueba.dat");
+        funcion.mostrarFichero("Prueba2.dat");
 
         System.out.println();
 
@@ -67,7 +69,7 @@ public class TestFuncionesFicheros {
         System.out.println();
 
         //ordenarPorPaisYNombre
-        System.out.println(funcion.ordenarPorPaisYNombre("Prueba.dat"));
-        funcion.mostrarFichero("Prueba.dat");
+        System.out.println(funcion.ordenarPorPaisYNombre("Prueba2.dat"));
+        funcion.mostrarFichero("Prueba2.dat");
     }
 }
